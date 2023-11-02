875d845eba9dc8dce667f4413f48b160f766c732
mjerick3 <mjerick3@ncsu.edu>
2023-11-02 17:17:55 -0400

    Added wolfpack_agent

--- run.py
+++ run.py
@@ -22,8 +22,8 @@ settings = {
             "parameters": {"storage_dir": "agent_storage/DreamTeam109Agent"},
         },
         {
-            "class": "agents.template_agent.template_agent.TemplateAgent",
-            "parameters": {"storage_dir": "agent_storage/TemplateAgent"},
+            "class": "agents.wolfpack_agent.wolfpack_agent.WolfpackAgent",
+            "parameters": {"storage_dir": "agent_storage/WolfpackAgent"},
         },
     ],
     "profiles": ["domains/domain00/profileA.json", "domains/domain00/profileB.json"],
